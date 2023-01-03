8261e321c120e112123f75557cc1a77723cec396
Dihee <hwangjihee94@gmail.com>
2023-01-03 13:07:15 +0900

    add: font

---
+++ Tabling/Tabling/Presentation/Common/Font.swift
@@ -0,0 +1,22 @@
+//
+//  Font.swift
+//  Tabling
+//
+//  Created by Jihee hwang on 2023/01/03.
+//
+
+import Foundation
+
+struct Font {
+    enum Roboto {
+        static let bold = "Roboto-Bold"
+        static let medium = "Roboto-Medium"
+        static let regular = "Roboto-Regular"
+    }
+    
+    enum NotoSans {
+        static let bold = "NotoSansKR-Bold"
+        static let medium = "NotoSansKR-Medium"
+        static let regular = "NotoSansKR-Regular"
+    }
+}
