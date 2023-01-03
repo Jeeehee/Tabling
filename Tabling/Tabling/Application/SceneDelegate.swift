72dbe0e5637c4e90dfcad35518a6252d48d2b91c
Dihee <hwangjihee94@gmail.com>
2023-01-03 12:57:45 +0900

    chore: Create Project

---
+++ Tabling/Tabling/Application/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  Tabling
+//
+//  Created by Jihee hwang on 2023/01/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ Tabling/Tabling/Presentation/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Tabling
+//
+//  Created by Jihee hwang on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
