c0c4a198c8f4b04b366ef34435916dc3bb9e78ca
Dihee <hwangjihee94@gmail.com>
2023-01-05 20:09:00 +0900

    feat: #3 FetchImageUseCase 구현

---
+++ Tabling/Tabling/Domain/FetchImageUseCase.swift
@@ -0,0 +1,23 @@
+//
+//  FetchImageUseCase.swift
+//  Tabling
+//
+//  Created by Jihee hwang on 2023/01/05.
+//
+
+import Foundation
+
+final class FetchImageUseCase {
+    private let repository: ImageRepository
+
+    init(repository: ImageRepository) {
+        self.repository = repository
+    }
+    
+    func start(_ url: String, completionHandler: @escaping (Data) -> Void) {
+        repository.loadImage(url) { imageData in
+            guard let imageData = imageData else { return }
+            completionHandler(imageData)
+        }
+    }
+}
