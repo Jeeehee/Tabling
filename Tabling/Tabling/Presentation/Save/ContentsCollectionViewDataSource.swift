1fa2b477ba5f5325ddb891ac20233de602992b91
Dihee <hwangjihee94@gmail.com>
2023-01-06 13:45:18 +0900

    feat: #4 CollectionViewDataSource와 Cell에 Data 주입 로직 구현 및 UI 수정

--- Tabling/Tabling/Presentation/Save/ContentsCollectionViewDataSource.swift
+++ Tabling/Tabling/Presentation/Save/ContentsCollectionViewDataSource.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 final class ContentsCollectionViewDataSource: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    var restaurantList: [Restaurant]?
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        2
+        restaurantList?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SaveCollectionViewCell.identifier, for: indexPath) as? SaveCollectionViewCell else { return UICollectionViewCell() }
-        
+        let data = fetchRestaurantList(index: indexPath)
+        cell.configureCellData(data)
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-    }
-    
+    // MARK: FlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 130)
+        return CGSize(width: collectionView.bounds.width, height: 130)
     }
-    
+}
+
+// MARK: Custom Method
+extension ContentsCollectionViewDataSource {
+    private func fetchRestaurantList(index: IndexPath) -> Restaurant? {
+        return restaurantList?[index.item]
+    }
 }
--- Tabling/Tabling/Presentation/Save/View/SaveCollectionViewCell.swift
+++ Tabling/Tabling/Presentation/Save/View/SaveCollectionViewCell.swift
@@ -7,22 +7,62 @@
 
 import UIKit
 
-class SaveCollectionViewCell: UICollectionViewCell {
+final class SaveCollectionViewCell: UICollectionViewCell {
     static var identifier: String {
         return "\(self)"
     }
-    
+    @IBOutlet weak var contentStackView: UIStackView!
+    @IBOutlet weak var detailInfoStackView: UIStackView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var gradeLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var numberOfReviewLabel: UILabel!
     @IBOutlet weak var watingLabel: UILabel!
+    @IBOutlet weak var badgeLabel: PaddingLabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureViews()
         backgroundColor = .white
         imageView.backgroundColor = .black
+        
+    }
+    
+    private func configureViews() {
+        contentStackView.setCustomSpacing(10, after: detailInfoStackView)
+        detailInfoStackView.setCustomSpacing(15, after: numberOfReviewLabel)
+        imageView.layer.cornerRadius = 10
+        
+        categoryLabel.font = .init(name: Font.NotoSans.regular, size: 13)
+        locationLabel.font = .init(name: Font.NotoSans.regular, size: 13)
+        nameLabel.font = .init(name: Font.NotoSans.medium, size: 21)
+        ratingLabel.font = .init(name: Font.NotoSans.regular, size: 14)
+        numberOfReviewLabel.font = .init(name: Font.NotoSans.regular, size: 14)
+        watingLabel.font = .init(name: Font.NotoSans.regular, size: 14)
+        setUpBadgeLabel()
+    }
+    
+    private func setUpBadgeLabel() {
+        badgeLabel.font = .init(name: Font.NotoSans.medium, size: 13)
+        badgeLabel.textColor = .tablingRed
+        badgeLabel.layer.cornerRadius = 16
+        badgeLabel.padding = UIEdgeInsets(top: 0, left: 18, bottom: 0, right: 18)
+        badgeLabel.layer.borderWidth = 1
+        badgeLabel.layer.borderColor = UIColor.tablingRed!.cgColor
+    }
+}
+
+extension SaveCollectionViewCell {
+    func configureCellData(_ model: Restaurant?) {
+        guard let model = model else { return }
+        
+        categoryLabel.text = model.classification
+        locationLabel.text = model.summaryAddress
+        nameLabel.text = model.restaurantName
+        ratingLabel.text = "\(model.rating)"
+        numberOfReviewLabel.text = "(\(model.reviewCount))"
+        watingLabel.text = "대기 \(model.waitingCount)명"
     }
 }
