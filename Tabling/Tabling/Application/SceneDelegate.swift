3014ce0ce6da92211d6b92c453b550636cb8ed28
Dihee <hwangjihee94@gmail.com>
2023-01-08 17:32:59 +0900

    feat: #4 ViewController에 ViewModel, UseCase 의존성 주입

--- Tabling/Tabling/Application/SceneDelegate.swift
+++ Tabling/Tabling/Application/SceneDelegate.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private let container = DIContainer()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let storyboard = UIStoryboard(name: "SaveView", bundle: nil)
+        let viewModel = container.makeListViewModel()
+        let viewController = storyboard.instantiateViewController(identifier: "SaveView") { coder -> SaveViewController? in
+            return .init(coder: coder, viewModel: viewModel) ?? SaveViewController(coder: coder, viewModel: viewModel)
+        }
+        window?.rootViewController = viewController
     }
 }
--- Tabling/Tabling/Presentation/Save/View/SaveViewController.swift
+++ Tabling/Tabling/Presentation/Save/View/SaveViewController.swift
@@ -16,22 +16,39 @@ class SaveViewController: UIViewController {
     
     private let tabCollectionViewDelegateNDataSource = TabCollectionViewDataSource()
     private let contentCollectionViewDelegateNDataSource = ContentsCollectionViewDataSource()
-    
-    convenience init(viewModel: ListViewModelProtocol) {
-        self.init()
+
+    init?(coder: NSCoder, viewModel: ListViewModelProtocol) {
         self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configureView()
+        guard let viewModel = viewModel else { return }
+
+        setupViews()
         setUpTabCollectionView()
         setUpContentsCollectionView()
-        viewModel?.input.viewDidLoad()
+        bind(to: viewModel)
+        viewModel.input.viewDidLoad()
     }
     
-    private func configureView() {
+    private func bind(to viewModel: ListViewModelProtocol) {
+        viewModel.items.observe(on: self) { _ in
+            self.contentCollectionViewDelegateNDataSource.viewModel = viewModel
+            self.contentCollectionViewDelegateNDataSource.imageUseCase = viewModel.imageUseCase.value
+            
+            DispatchQueue.main.async {
+                self.contentsCollectionView.reloadData()
+            }
+        }
+    }
+    
+    private func setupViews() {
         titleLabel.text = "관심 매장"
         titleLabel.font = .init(name: Font.NotoSans.bold, size: 25)
     }
@@ -39,6 +56,7 @@ class SaveViewController: UIViewController {
     private func setUpTabCollectionView() {
         tabCollectionView.delegate = tabCollectionViewDelegateNDataSource
         tabCollectionView.dataSource = tabCollectionViewDelegateNDataSource
+        tabCollectionViewDelegateNDataSource.delegate = self
         
         let nibName = UINib(nibName: TabCollectionViewCell.identifier, bundle: nil)
         tabCollectionView.register(nibName, forCellWithReuseIdentifier: TabCollectionViewCell.identifier)
@@ -48,11 +66,22 @@ class SaveViewController: UIViewController {
     }
     
     private func setUpContentsCollectionView() {
-        contentsCollectionView.backgroundColor = .lightGray
         contentsCollectionView.delegate = contentCollectionViewDelegateNDataSource
         contentsCollectionView.dataSource = contentCollectionViewDelegateNDataSource
         
         let nibName = UINib(nibName: SaveCollectionViewCell.identifier, bundle: nil)
         contentsCollectionView.register(nibName, forCellWithReuseIdentifier: SaveCollectionViewCell.identifier)
+    }
+    
+    private func updateItems() {
+        DispatchQueue.main.async {
+            self.contentsCollectionView.reloadData()
+        }
+    }
+}
+
+extension SaveViewController: TabCollectionViewDelegate {
+    func scrollToIndex(to index: Int) {
+        viewModel?.didChangeTab(index)
     }
 }
